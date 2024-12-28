12646918628b54cb5da1757ff8a1043028c67d89
AMIK-PYGAME <amikahnaf86@gmail.com>
2024-12-28 15:51:50 +0530

    Add files via upload

---
+++ physics sim/physics_objects.py
@@ -0,0 +1,102 @@
+import pygame
+import math 
+pygame.init()
+clock = pygame.time.Clock()
+class physics_box(pygame.sprite.Sprite):
+    
+    def __init__(
+                 self,group,mass,position:tuple,image,gravity:bool,boundaries:bool,movement:bool,
+                 air_resistance:bool,initial_velocity:tuple,constant_force:tuple,type_of_motion:str
+                 ):
+        super().__init__(group)
+        self.dt = dt = clock.tick() / 1000
+        self.image = image
+        self.rect = self.image.get_frect(topleft = position)
+        self.window_width,self.window_height = pygame.display.get_window_size() 
+        self.position = position 
+        self.boundaries = boundaries
+        self.gravity = gravity
+        self.movement = movement
+        self.constant_force = pygame.math.Vector2(constant_force)
+        self.normal_reaction = pygame.math.Vector2()
+        self.velocity = pygame.math.Vector2(initial_velocity)
+        self.air_resistance = pygame.math.Vector2()
+        self.all_vectors = []
+        self.air_density = 1.2278
+        self.drag_coefficient = 1.22
+        self.mass = mass
+        self.apply_resistance = air_resistance
+        self.check = [ (i) for i in range(0,100)]
+    
+    def update(self):
+        """the update function is called each frame 
+        .this is where every function is called
+        this can help to arrange the order in which the functions
+        are called.for more info about the function go to the respective 
+        function."""
+       
+        self.dt = clock.tick() / 1000
+        self.calc_resulting_vector(self.dt)
+        if self.boundaries:
+            self.independant_vector_normal_reaction()
+        
+        if self.movement :
+            self.move(self.dt)
+        
+        if self.gravity:
+            self.independant_vector_gravity()
+
+    def independant_vector_normal_reaction(self):
+     """this function acts as a reaction force whenever a body
+     collides with a boundary. this function does not change the 
+     magnitude but only multiplies the velocity vector by -1 thus making
+     the body go in the opposite direction"""
+     
+     if self.rect.bottom > self.window_height:
+        self.velocity.y *= -1
+        self.rect.bottom = self.window_height
+     elif self.rect.top < 0:
+        self.velocity.y *= -1
+        self.rect.top = 0
+     if self.rect.left < 0:
+        self.velocity.x *= -1
+        self.rect.left= 0
+     elif self.rect.right > self.window_width:
+        self.velocity.x *= -1
+        self.rect.right = self.window_width 
+
+    def move(self,dt):
+        self.rect.center += self.velocity*dt 
+   
+    def independant_vector_gravity(self):
+        pass
+    
+    def calc_resulting_vector(self,dt):
+        self.velocity += ((self.constant_force)  + (self.calc_air_resistance()))/self.mass
+    
+    def calc_totall_energy_in_system(self):
+        displacement = (self.position-self.rect.center).length()
+        potential_energy = self.constant_force * displacement
+        
+    def calc_air_resistance(self):
+     
+     if self.apply_resistance:
+     
+      """Calculate air resistance based on the drag force formula.
+      [drag] = c*(v)*d*a
+       where [] stands for magnitude"""
+      
+      angle = math.atan2(self.velocity.y, self.velocity.x)
+      cross_sec_length = self.image.get_width() * abs(math.cos(angle))
+    
+      velocity_squared = self.velocity.length()
+      drag = -0.001 * self.air_density * velocity_squared * self.drag_coefficient * cross_sec_length
+      
+      #calc the resistance vector by multiplying 
+      #velocity's unit vector to the drag
+      resistance_vector = (self.velocity/self.velocity.length()) *drag
+      return resistance_vector
+     
+     return pygame.math.Vector2(0,0)
+
+    
